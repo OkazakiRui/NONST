633599b37b41702d969f6c007d381d74dd44fda3
RuiOkazaki <okazaki200226@gmail.com>
2021-09-13 02:01:35 +0900

    Mypage style create

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="footer-pb">
     <app-header text="マイページ" />
+    <div class="mypage f-ali">
+      <div class="mypage__userImgWrap f-ali">
+        <img class="mypage__userImg" :src="userImg" />
+        <button class="mypage__userImgButton" @click="userIconEdit">
+          プロフィール写真を変更
+        </button>
+        <input
+          class="mypage__userImgInput"
+          id="userIconInput"
+          type="file"
+          accept="image/*"
+        />
+      </div>
+
+      <div class="f-alibet mypage__details">
+        <h4 class="mypage__userName">{{ userName }}</h4>
+        <p class="mypage__userAgeAndLive">{{ userAge + "・" + userLive }}</p>
+      </div>
+
+      <div class="mypage__userMessage">
+        <label>
+          <h4 class="mypage__title">ひとこと</h4>
+          <textarea :placeholder="userMessage"></textarea>
+        </label>
+      </div>
+    </div>
     <app-footer selected="4" />
   </div>
 </template>
@@ -10,10 +36,28 @@ import AppHeader from "../components/AppHeader.vue";
 import AppFooter from "../components/AppFooter.vue";
 
 export default {
+  data() {
+    return {
+      userName: "sample 太郎",
+      userAge: "30代",
+      userLive: "兵庫県",
+      userMessage:
+        "35歳の限界社畜です。上司がうざすぎてはじめました。よろしくおねいがします。",
+      userImg: "./img/userIcon.svg",
+    };
+  },
   components: {
     AppHeader,
     AppFooter,
   },
-  methods: {},
+  methods: {
+    userIconEdit() {
+      document.getElementById("userIconInput").click();
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../scss/mypage.scss";
+</style>
