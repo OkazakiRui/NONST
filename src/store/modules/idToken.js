c7566b950b3d3bb58e9dce6bcc667bf92638a227
RuiOkazaki <okazaki200226@gmail.com>
2021-09-14 22:20:04 +0900

    トークンを自動更新する

--- src/store/modules/idToken.js
+++ src/store/modules/idToken.js
@@ -1,4 +1,5 @@
 import axios from "../../axios-auth";
+import axiosRefresh from "../../axios-refresh";
 import router from "../../router";
 
 const state = {
@@ -10,7 +11,7 @@ const mutations = {
   },
 };
 const actions = {
-  signup({ commit }, authData) {
+  signup({ commit, dispatch }, authData) {
     axios
       .post(`/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_APIKEY}`, {
         email: authData.email,
@@ -19,6 +20,11 @@ const actions = {
       })
       .then((response) => {
         commit("updateIdToken", response.data.idToken);
+        // 有効期限の時間が来ると一回実行
+        setTimeout(() => {
+          // トークンをリフレッシュする関数
+          dispatch("refreshIdToken", response.data.refreshToken);
+        }, response.data.expiresIn * 1000);
         router.push({ path: "/CreateAccount" });
       });
   },
@@ -35,6 +41,21 @@ const actions = {
       .then((response) => {
         commit("updateIdToken", response.data.idToken);
         router.push({ path: "/" });
+      });
+  },
+  // トークンをリフレッシュする関数
+  refreshIdToken({ commit, dispatch }, refreshToken) {
+    axiosRefresh
+      .post(`/token?key=${process.env.VUE_APP_FIREBASE_APIKEY}`, {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+      })
+      .then((response) => {
+        commit("updateIdToken", response.data.id_token);
+        // 有効期限の時間が来るたびに自動で延長
+        setTimeout(() => {
+          dispatch("refreshIdToken", response.data.refresh_token);
+        }, response.data.expires_in * 1000);
       });
   },
 };
