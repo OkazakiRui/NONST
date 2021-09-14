a1b861e5c64f15e6019f5b3338ca9864ecb68aa3
RuiOkazaki <okazaki200226@gmail.com>
2021-09-14 22:41:18 +0900

    ログイン継続

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-
-axios.defaults.baseURL = `https://firestore.googleapis.com/v1/projects/${process.env.VUE_APP_FIREBASE_PROJECT_ID}/databases/(default)/documents`;
-
-// ress 読み込み
 import "ress";
 
+// baseURL setting
+axios.defaults.baseURL = `https://firestore.googleapis.com/v1/projects/${process.env.VUE_APP_FIREBASE_PROJECT_ID}/databases/(default)/documents`;
+// autoSignin を実行
+store.dispatch("autoSignin");
 createApp(App).use(store).use(router).mount("#app");
--- src/store/modules/idToken.js
+++ src/store/modules/idToken.js
@@ -19,7 +19,16 @@ const actions = {
         returnSecureToken: true,
       })
       .then((response) => {
+        const now = new Date();
+        // 有効期限が切れる時間を代入
+        const expiryTimeMs = now.getTime() + response.data.expiresIn * 1000;
+
+        // localStorage に保存
+        localStorage.setItem("idToken", response.data.idToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+        localStorage.setItem("expiryTimeMs", expiryTimeMs);
         commit("updateIdToken", response.data.idToken);
+
         // 有効期限の時間が来ると一回実行
         setTimeout(() => {
           // トークンをリフレッシュする関数
@@ -28,7 +37,7 @@ const actions = {
         router.push({ path: "/CreateAccount" });
       });
   },
-  signin({ commit }, authData) {
+  signin({ commit, dispatch }, authData) {
     axios
       .post(
         `/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_APIKEY}`,
@@ -39,7 +48,11 @@ const actions = {
         }
       )
       .then((response) => {
+        localStorage.setItem("idToken", response.data.idToken);
         commit("updateIdToken", response.data.idToken);
+        setTimeout(() => {
+          dispatch("refreshIdToken", response.data.refreshToken);
+        }, response.data.expiresIn * 1000);
         router.push({ path: "/" });
       });
   },
@@ -51,12 +64,35 @@ const actions = {
         refresh_token: refreshToken,
       })
       .then((response) => {
+        // localStorage.setItem("idToken", response.data.id_token);
         commit("updateIdToken", response.data.id_token);
         // 有効期限の時間が来るたびに自動で延長
         setTimeout(() => {
           dispatch("refreshIdToken", response.data.refresh_token);
         }, response.data.expires_in * 1000);
       });
+  },
+  autoSignin({ commit, dispatch }) {
+    const idToken = localStorage.getItem("idToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    const expiryTimeMs = localStorage.getItem("expiryTimeMs");
+    if (!idToken) return;
+    const now = new Date();
+    // 有効期限が切れているかを判別
+    // 切れているとtrue
+    const isExpired = now.getTime() >= expiryTimeMs;
+
+    if (isExpired) {
+      dispatch("refreshIdToken", refreshToken);
+    } else {
+      const expiresInMs = expiryTimeMs - now.getTime();
+      // 残り時間秒後にTokenを更新する
+      setTimeout(() => {
+        dispatch("refreshIdToken", refreshToken);
+      }, expiresInMs);
+
+      commit("updateIdToken", idToken);
+    }
   },
 };
 const getters = {
