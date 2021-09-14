83a14f01a16ed1419e337a356bf45faa999a9358
RuiOkazaki <okazaki200226@gmail.com>
2021-09-14 15:45:16 +0900

    axios install

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-// import axios from "axios";
 import store from "./store";
 
 // ress 読み込み
