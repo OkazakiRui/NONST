08f680b6f91bb7c993f7cfd6f3d73e3121d41bd8
RuiOkazaki <okazaki200226@gmail.com>
2021-09-15 00:01:02 +0900

    signin err message add

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -5,17 +5,21 @@
       <input-text
         text="example@example.com"
         title="メールアドレス"
-        :iconPath="iconPath.mail"
+        :errorMessage="errorMessage.email"
+        :iconPath="iconPath.email"
         class="inputText"
         v-model="user.email"
+        @my-change="inputUpdate"
       />
       <input-text
         text="半角英数字で8文字以上"
         title="パスワード"
+        :errorMessage="errorMessage.password"
         :iconPath="iconPath.password"
         type="password"
         class="inputText"
         v-model="user.password"
+        @my-change="inputUpdate"
       />
     </div>
     <Button1
@@ -44,19 +48,49 @@ export default {
         email: "",
         password: "",
       },
+      errorMessage: {
+        email: "",
+        password: "",
+      },
+      errorMessageList: {
+        email: "※無効なメールアドレスです。",
+        password: "※半角英数字8文字以上にしてください。",
+      },
       iconPath: {
-        mail: "./img/mail.svg",
+        email: "./img/email.svg",
         password: "./img/password.svg",
       },
-      press: true,
+      press: false,
     };
   },
   methods: {
     signin() {
-      this.$store.dispatch("signin", {
-        email: this.user.email,
-        password: this.user.password,
-      });
+      if (this.press) {
+        this.$store.dispatch("signin", {
+          email: this.user.email,
+          password: this.user.password,
+        });
+      }
+    },
+    inputUpdate() {
+      const emailReg =
+        /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+      const passwordReg = /^([a-zA-Z0-9]{8,})$/;
+      if (this.user.email && this.user.password) {
+        if (this.user.email.match(emailReg)) this.errorMessage.email = "";
+        else this.errorMessage.email = this.errorMessageList.email;
+
+        if (this.user.password.match(passwordReg))
+          this.errorMessage.password = "";
+        else this.errorMessage.password = this.errorMessageList.password;
+
+        if (
+          this.errorMessage.email == false &&
+          this.errorMessage.password == false
+        )
+          this.press = true;
+        else this.press = false;
+      }
     },
   },
 };
